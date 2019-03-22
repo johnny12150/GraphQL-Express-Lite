1de16199c65ad668df47ed5e1eef833633ec7aae
wade <johnny12150@gmail.com>
2019-03-22 10:41:22 +0800

    Fetch Rancher stack API

--- README.md
+++ README.md
@@ -6,5 +6,16 @@
 
 > https://graphql.org/graphql-js/running-an-express-graphql-server/
 
+### 套件
+> https://ithelp.ithome.com.tw/articles/10188294
+* graphql 是實作了 GraphQL 核心的 JavaScript 套件負責解析 AST、執行 Validation、建構基礎的 Type 與 Schema 等
+* express-graphql 則是讓 Express 可以處理 GraphQL Schema 的套件
+* express做web server (API server)
+* node-fetch做fetch API
+
+### 檔案架構
+1. server-test.js為測試與學習用
+2. server.js會嘗試整合Rancher1.6的API
+
 ### 執行
 Running a GraphQL API server at ` http://localhost:4000/graphql`
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var {buildSchema} = require('graphql');
+const fetch = require('node-fetch');
 
 // schema
 var schema = buildSchema(`
@@ -9,8 +10,26 @@ var schema = buildSchema(`
     id: Int
   }
   
+  type list {
+    text: String,
+    id: Int
+  }
+  
+  type stack {
+    id: String,
+    type: String,
+    name: String,
+    state: String,
+    created: String,
+    description: String
+  }
+  
   type Query {
-    hello(id: Int!): data
+    hello(id: Int!): data,
+    "顯示完整的array"
+    list: [list],
+    "取Rancher 1.6 API stack的基礎資料"
+    stack: stack
   }
 `);
 
@@ -28,11 +47,20 @@ let fake_data = [
 
 // resolver
 var root = {
+    // 對應到 shema下的 Query的 field name為 hello
     hello: (source, args) => {
         // source 是 client端帶入的參數
         console.log(source);
         return fake_data[source.id]
     },
+    list: () => {
+        // 直接回傳array
+        return fake_data;
+    },
+    stack: () => {
+        // fetch Rancher的stack API
+        return fetch('http://192.168.1.43/v2-beta/projects/1a5/stacks/1st47').then(res => res.json())
+    }
 };
 
 var app = express();
